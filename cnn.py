7bc92361ff78a4406c597d5104416e0baddf06b5
Krishya <111554979+Krishya0215@users.noreply.github.com>
2024-11-17 17:06:54 +0800

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -251,6 +251,9 @@ def test_model(input_audio_path, output_audio_path, model, device, sample_rate=4
     output_imag = output_value[num_freq_bins:]
     reconstructed_spectrum = output_real + 1j * output_imag
 
+    # 恢复 19000 Hz 以下的频率为零
+    reconstructed_spectrum = np.concatenate([np.zeros(cutoff_index), reconstructed_spectrum])
+
     reconstructed_waveform = np.fft.ifft(reconstructed_spectrum).real
     reconstructed_waveform = (reconstructed_waveform * 32767).astype(np.int16)
     output_audio = AudioSegment(
